d763d0b3ffe7058d0265f1f555bfcbc4eb071691
timkellogg <tim.kellogg@gmail.com>
2015-08-21 09:28:08 -0700

    finish testing for attributes of stylist

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -9,4 +9,22 @@ describe Stylist do
 			expect(@stylist.fname).to eq 'Sara'
 		end
 	end
+
+	describe '#lname' do 
+		it 'should return the last name of the stylist' do 
+			expect(@stylist.lname).to eq 'Adams'
+		end
+	end
+
+	describe '#style' do  
+		it 'should return the hair style of the stylist' do 
+			expect(@stylist.style).to eq 'modern'
+		end
+	end
+
+	describe '#id' do  
+		it 'should return the id of the stylist' do 
+			expect(@stylist.id).to eq nil 
+		end
+	end
 end
