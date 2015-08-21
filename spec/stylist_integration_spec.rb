2f189fd53c34000bcb1a9d8f7b84246589eafc71
timkellogg <tim.kellogg@gmail.com>
2015-08-21 15:47:46 -0700

    fix integration specs after layout changes

--- spec/stylist_integration_spec.rb
+++ spec/stylist_integration_spec.rb
@@ -7,33 +7,29 @@ describe 'when navigating to the home page', { type: :feature } do
 	before { visit '/' }
 
 	it 'should display navigation options' do
-		expect(page).to have_content 'member'
-		expect(page).to have_content 'stylists'
+		expect(page).to have_content 'Stylists'
 	end
 
 	it 'should allow the user to navigate to a list of all stylists' do
-		click_link('stylists')
+		click_link 'Stylists'
 		expect(page).to have_content 'Current Stylists:'
-	end
-
-	it 'should allow the user to navigate to a list of all clients' do
-		click_link('clients')
-		expect(page).to have_content 'Current Clients:'
 	end
 end
 
 describe 'when creating a stylist', { type: :feature } do
 
-	before { visit '/stylist/new' }
+	before { visit '/' }
 
 	it 'should render a form' do
+		click_link 'Stylists'
 		expect(page).to have_content 'Add a new stylist'
-		expect(page).to have_content 'First name:'
-		expect(page).to have_content 'Last name:'
+		expect(page).to have_content 'First Name:'
+		expect(page).to have_content 'Last Name:'
 		expect(page).to have_content 'Styles:'
 	end
 
 	it 'should create a new stylist when submitted and render the page of all stylists' do
+		visit '/stylist/new'
 		fill_in 'fname',  with: 'Joe'
 		fill_in 'lname',  with: 'Pesci'
 		fill_in 'styles', with: 'modern'
