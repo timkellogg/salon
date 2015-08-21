95afb5b157c35b43bbb6f83694f7071f006d8e96
timkellogg <tim.kellogg@gmail.com>
2015-08-21 11:08:43 -0700

    DRY up integration specs by adding before blocks

--- spec/site_integration_spec.rb
+++ spec/site_integration_spec.rb
@@ -3,15 +3,30 @@ require 'spec_helper'
 Capybara.app = Sinatra::Application
 
 describe 'when navigating to the home page', { type: :feature } do
+
+	before { visit '/' }
+
 	it 'should display navigation options' do
-		visit '/'
 		expect(page).to have_content 'member'
 		expect(page).to have_content 'stylists'
 	end
 
-	it 'should display a list of all current stylists' do
-		visit '/'
+	it 'should allow the user to navigate to a list of all stylists' do
 		click_link('stylists')
 		expect(page).to have_content 'Current Stylists:'
+	end
+
+	it 'should allow the user to navigate to a list of all clients' do
+		click_link('clients')
+		expect(page).to have_content 'Current Clients:'
+	end
+end
+
+describe 'when creating a stylist', { type: :feature } do
+
+	before { visit '/stylists' }
+
+	it 'should render a form' do
+		expect(page).to have_content 'Current Stylists'
 	end
 end
